5c428fbcba8a6d2b834dd275ebf3072ea9278447
Juan-Vieira <juan.reis@sptech.school>
2025-05-16 14:15:20 -0300

    Atualização no banco de dados (incompleto)

--- Banco de dados/Script - MorumByte.sql
+++ Banco de dados/Script - MorumByte.sql
@@ -9,11 +9,37 @@ email VARCHAR(50) UNIQUE,
 senha CHAR(11)
 );
 
-CREATE TABLE tentativas_quiz (
-idTentativa INT,
+CREATE TABLE perguntas_quiz (
+idPerguntas INT PRIMARY KEY,
+enunciado VARCHAR(200),
 fkUsuario INT,
-CONSTRAINT pkUsuarioTentativa PRIMARY KEY (idTentativa, fkUsuario),
-CONSTRAINT fkUsuarioTentativa FOREIGN KEY (fkUsuario) REFERENCES usuario (idUsuario),
+CONSTRAINT fkUsuarioPergunta FOREIGN KEY (fkUsuario) REFERENCES usuario (idUsuario)
+);
+
+CREATE TABLE alternativas_quiz (
+idAlternativas INT PRIMARY KEY,
+enunciado VARCHAR(100),
+correta BOOLEAN,
+fkPerguntas INT,
+CONSTRAINT fkPerguntasAlternativas FOREIGN KEY (fkPerguntas) REFERENCES perguntas_quiz (idPerguntas)
+);
+
+CREATE TABLE resposta_quiz (
+idRespostas INT PRIMARY KEY,
+correta BOOLEAN,
+fkUsuario INT,
+fkPerguntas INT,
+fkAlternativas INT, 
+CONSTRAINT fkRespostaQuiz 
+FOREIGN KEY (fkUsuario) REFERENCES usuario (idUsuario),
+FOREIGN KEY (fkPerguntas) REFERENCES perguntas_quiz (idPerguntas),
+FOREIGN KEY (fkAlternativas) REFERENCES alternativas_quiz (idAlternativas)
+);
+
+CREATE TABLE resultados_quiz (
+idResultados INT PRIMARY KEY,
 acertos INT,
-erros INT
+erros INT,
+fkUsuario INT,
+CONSTRAINT fkUsuarioResultado FOREIGN KEY (fkUsuario) REFERENCES usuario (idUsuario)
 );
