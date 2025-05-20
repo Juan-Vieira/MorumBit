c3cc468d6e9ca17a9d9af313df921ff4fc5d8168
Juan-Vieira <juan.reis@sptech.school>
2025-05-20 00:26:30 -0300

    página de perfil com os dados do usuário finalizado

--- Site/src/controllers/usuarioController.js
+++ Site/src/controllers/usuarioController.js
@@ -25,6 +25,44 @@ function cadastrar(req, res) {
         );
 }
 
+function autenticar(req, res) {
+    var email = req.body.emailServer;
+    var senha = req.body.senhaServer;
+
+    usuarioModel.autenticar(email, senha)
+        .then(
+            function (resultadoAutenticar) {
+                console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+                console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+
+                if (resultadoAutenticar.length == 1) {
+
+                    console.log(resultadoAutenticar);
+                    res.json({
+                        username: resultadoAutenticar[0].username,
+                        dtNasc: resultadoAutenticar[0].dtNasc,
+                        email: resultadoAutenticar[0].email,
+                        senha: resultadoAutenticar[0].senha
+                    });
+
+                } else if (resultadoAutenticar.length == 0) {
+                    res.status(403).send("Email e/ou senha inválido(s)");
+                } else {
+                    res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+                }
+            }
+        ).catch(
+            function (erro) {
+                console.log(erro);
+                console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
+
+
+
 module.exports = {
-    cadastrar
+    cadastrar,
+    autenticar
 }
--- Site/src/models/usuarioModel.js
+++ Site/src/models/usuarioModel.js
@@ -3,8 +3,6 @@ var database = require("../database/config")
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
 function cadastrar(username, dtNasc, email, senha) {
 
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
     var instrucaoSql = `
         INSERT INTO usuario (username, dtNascimento, email, senha) VALUES ('${username}', '${dtNasc}', '${email}', '${senha}');
     `;
@@ -12,6 +10,16 @@ function cadastrar(username, dtNasc, email, senha) {
     return database.executar(instrucaoSql);
 }
 
+function autenticar(email, senha) {
+    
+    var instrucaoSql = `
+        SELECT username, dtNascimento, email, senha FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
 module.exports = {
-    cadastrar
+    cadastrar,
+    autenticar
 };
