b1aed4bb621fbbf12519bead91799c744e34a599
Juan-Vieira <juan.reis@sptech.school>
2025-05-30 01:09:05 -0300

    Atualização no banco de dados

--- Site/src/database/Script - MorumByte.sql
+++ Site/src/database/Script - MorumByte.sql
@@ -1,119 +1,89 @@
 create database morumbyte;
 use morumbyte;
 
--- Tabela com os dados do usuário
+-- Dados do usuário
 CREATE TABLE usuario (
     idUsuario INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(20) NOT NULL UNIQUE,
     dtNascimento DATE,
     email VARCHAR(30) NOT NULL UNIQUE,
-    senha VARCHAR(11) NOT NULL
+    senha VARCHAR(60) NOT NULL
 );
 
--- Tabela com o número da questão do quiz
+-- Dados da questão do quiz
 CREATE TABLE questoes_quiz (
     idQuestao INT AUTO_INCREMENT PRIMARY KEY,
     numero INT
 );
 
--- Tabela com os dados referentes a tentativa do usuário
+-- Dados da tentativa do usuário no quiz
 CREATE TABLE tentativas_quiz (
-    idTentativa INT AUTO_INCREMENT PRIMARY KEY,
-    fkUsuario INT NOT NULL,
+    idTentativa INT PRIMARY KEY AUTO_INCREMENT,
     acertos INT,
-    FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario)
+    fkUsuario INT,
+    CONSTRAINT fkUsuarioTentativa FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario)
 );
 
--- Tabela com a verificação de acerto ou não (TRUE ou FALSE) de cada questão na determinada tentativa
-CREATE TABLE respostas_tentativa (
-    idResposta INT AUTO_INCREMENT PRIMARY KEY,
-    fkTentativa INT NOT NULL,
-    fkQuestao INT NOT NULL,
-    acertou BOOLEAN NOT NULL,
+-- Dados das respostas do usuário no quiz
+CREATE TABLE respostas_quiz (
+    fkTentativa INT,
+    fkQuestao INT,
+    acertos TINYINT,
+    PRIMARY KEY (fkTentativa, fkQuestao),
     FOREIGN KEY (fkTentativa) REFERENCES tentativas_quiz(idTentativa),
-    FOREIGN KEY (fkQuestao) REFERENCES questoes(idQuestao)
+    FOREIGN KEY (fkQuestao) REFERENCES questoes_quiz(idQuestao)
 );
 
--- Exemplos de insert
-INSERT INTO usuario (idUsuario, username, dtNascimento, email, senha) VALUES 
-	(DEFAULT, 'joao silva', '1999-10-02', 'joao@email.com', 'ASHSEGURO01'),
-    (DEFAULT, 'juan vieira', '2000-05-10', 'juan@email.com', '$Hsfsfsfs02'),
-    (DEFAULT, 'ayane pereira', '2025-08-15', 'ayane@email.com', 'dcsdvs03'),
-    (DEFAULT, 'pedro alves', '1800-01-01', 'pedro@email.com', 'URO04');
-    
--- Definindo o número das questões
-INSERT INTO questoes (numero) VALUES 
-	(1),
-	(2),
-	(3),
-	(4),
-	(5);
+-- INSERTS (EXEMPLOS)
 
--- Definindo a quantidade de acertos do usuário por tentativa
-INSERT INTO tentativas_quiz (fkUsuario, acertos) VALUES 
-	(1, 4),
-    (1, 3),
-    (2, 1),
-    (2, 5),
-    (3, 3),
-    (4, 4);
+-- Inserindo usuários
+INSERT INTO usuario (username, dtNascimento, email, senha) VALUES 
+('user1', '2000-01-01', 'user1@email.com', 'senha123'),
+('user2', '2001-02-02', 'user2@email.com', 'senha456'),
+('user3', '2000-01-01', 'user3@email.com', 'senha123');
 
+-- Inserindo questões
+INSERT INTO questoes_quiz (numero) VALUES 
+(1), 
+(2), 
+(3);
 
--- Respostas do usuário em cada questão (1 até 5), referente a Tentativa 1 (ID = 1)
-INSERT INTO respostas_tentativa (fkTentativa, fkQuestao, acertou) VALUES 
-	(1, 1, TRUE),
-    (1, 2, FALSE),
-    (1, 3, TRUE),
-    (1, 4, TRUE),
-    (1, 5, TRUE);
+-- Inserindo tentativas
 
--- Respostas do usuário em cada questão (1 até 5), referente a Tentativa 2 (ID = 2)
-INSERT INTO respostas_tentativa (fkTentativa, fkQuestao, acertou) VALUES 
-	(2, 1, TRUE),
-    (2, 2, FALSE),
-    (2, 3, TRUE),
-    (2, 4, FALSE),
-    (2, 5, TRUE);
-    
--- Respostas do usuário em cada questão (1 até 5), referente a Tentativa 3 (ID = 3)
-INSERT INTO respostas_tentativa (fkTentativa, fkQuestao, acertou) VALUES 
-	(3, 1, FALSE),
-    (3, 2, FALSE),
-    (3, 3, FALSE),
-    (3, 4, FALSE),
-    (3, 5, TRUE);
-    
--- Respostas do usuário em cada questão (1 até 5), referente a Tentativa 4 (ID = 4)
-INSERT INTO respostas_tentativa (fkTentativa, fkQuestao, acertou) VALUES 
-	(4, 1, TRUE),
-    (4, 2, TRUE),
-    (4, 3, TRUE),
-    (4, 4, TRUE),
-    (4, 5, TRUE);
-    
--- Respostas do usuário em cada questão (1 até 5), referente a Tentativa 5 (ID = 5)
-INSERT INTO respostas_tentativa (fkTentativa, fkQuestao, acertou) VALUES 
-	(5, 1, FALSE),
-    (5, 2, FALSE),
-    (5, 3, TRUE),
-    (5, 4, TRUE),
-    (5, 5, TRUE);
-    
--- Respostas do usuário em cada questão (1 até 5), referente a Tentativa 6 (ID = 6)
-INSERT INTO respostas_tentativa (fkTentativa, fkQuestao, acertou) VALUES 
-	(6, 1, FALSE),
-    (6, 2, TRUE),
-    (6, 3, TRUE),
-    (6, 4, TRUE),
-    (6, 5, TRUE);
-    
+-- INÍCIO DO QUIZ
+INSERT INTO tentativas_quiz (acertos, fkUsuario) VALUES
+(0, 1);  -- user1 acertou 2 questões
+
+INSERT INTO tentativas_quiz (acertos, fkUsuario) VALUES
+(2, 1),  -- user1 acertou 2 questões
+(1, 2),  -- user2 acertou 1 questão
+(1, 3),  -- user3 acertou 1 questões
+(2, 2);  -- user2 acertou 2 questão
+
+-- Inserindo respostas
+INSERT INTO respostas_quiz (fkTentativa, fkQuestao, acertos) VALUES
+(1, 1, 1),  -- tentativa 1, questão 1, acertou
+(1, 2, 1),  -- tentativa 1, questão 2, acertou
+(1, 3, 0),  -- tentativa 1, questão 3, errou
+
+(2, 1, 0),  -- tentativa 2, questão 1, errou
+(2, 2, 0),  -- tentativa 2, questão 2, errou
+(2, 3, 1),  -- tentativa 2, questão 3, acertou
+
+(3, 1, 0),  -- tentativa 3, questão 1, errou
+(3, 2, 1),  -- tentativa 3, questão 2, acertou
+(3, 3, 0),  -- tentativa 3, questão 3, errou
+
+(4, 1, 1),  -- tentativa 4, questão 1, acertou
+(4, 2, 0),  -- tentativa 4, questão 2, errou
+(4, 3, 1);  -- tentativa 4, questão 3, acertou
+
+	
 -- Exibindo todos os dados
 select * from usuario;
 select * from tentativas_quiz;
-select * from questoes;
-select * from respostas_tentativa;
-
-truncate table usuario;
+select * from questoes_quiz;
+select * from respostas_quiz;
 
 -- UPDATE
 -- Atualizar Dados
@@ -122,57 +92,49 @@ UPDATE usuario SET username = "Reiz",
     email = "Reiz@gmail.com",
     senha = "Reiz123"
     WHERE idUsuario = 2;
+    
+-- Atualizar número de acertos da tentativa do quiz
+UPDATE tentativas_quiz SET acertos = 10
+WHERE idTentativa = 3 AND fkUsuario = 3;
 
 -- SELECTS
-
 -- Login
 
  SELECT idUsuario, username, dtNascimento, email, senha FROM usuario WHERE email = 'juan@gmail.com' AND senha = 'juan123';
 
 -- Número de usuários que respondeu o quiz
-SELECT count(distinct fkUsuario) from tentativas_quiz;
+SELECT COUNT(DISTINCT fkUsuario) AS usuarios_que_responderam
+FROM tentativas_quiz;
 
 -- Média de acertos dos usuários no quiz
-select avg(acertos) from tentativas_quiz;
+SELECT ROUND(AVG(acertos),0) AS media_acertos
+FROM tentativas_quiz;
 
 -- Taxa média de acertos por questão
-SELECT 
-	q.numero,
-    ROUND(SUM(r.acertou) * 100.0 / COUNT(r.idResposta), 2)
-FROM questoes q
-LEFT JOIN respostas_tentativa r ON q.idQuestao = r.fkQuestao
-GROUP BY q.numero;
+SELECT fkQuestao, 
+       ROUND(AVG(acertos) * 100, 2) AS taxa_media_acertos_percentual
+FROM respostas_quiz
+GROUP BY fkQuestao;
 
--- Desempenho dos ultimos 10 usuarios
-SELECT username, acertos FROM usuario JOIN tentativas_quiz ON idUsuario = fkUsuario;
+-- Desempenho dos usuarios
+SELECT t.idTentativa, u.username, SUM(t.acertos) AS total_acertos
+FROM usuario u
+JOIN tentativas_quiz t ON u.idUsuario = t.fkUsuario
+GROUP BY t.idTentativa, u.idUsuario
+ORDER BY t.idTentativa;
 
 -- Questão com maior porcentagem de acertos
-SELECT 
-    sub.numero,
-    sub.porcentagem_acertos
-FROM (
-    SELECT 
-        q.numero,
-        ROUND(SUM(r.acertou) * 100.0 / COUNT(r.idResposta), 2) AS porcentagem_acertos
-    FROM questoes q
-    LEFT JOIN respostas_tentativa r ON q.idQuestao = r.fkQuestao
-    GROUP BY q.idQuestao
-) AS sub
-ORDER BY sub.porcentagem_acertos DESC
+SELECT fkQuestao, 
+       ROUND(SUM(acertos) / COUNT(*) * 100, 2) AS porcentagem_acertos
+FROM respostas_quiz
+GROUP BY fkQuestao
+ORDER BY porcentagem_acertos DESC
 LIMIT 1;
 
-
 -- Questão com maior porcentagem de erros
-SELECT 
-    sub.numero,
-    sub.porcentagem_acertos
-FROM (
-    SELECT 
-        q.numero,
-        ROUND(SUM(r.acertou) * 100.0 / COUNT(r.idResposta), 2) AS porcentagem_acertos
-    FROM questoes q
-    LEFT JOIN respostas_tentativa r ON q.idQuestao = r.fkQuestao
-    GROUP BY q.idQuestao
-) AS sub
-ORDER BY sub.porcentagem_acertos
+SELECT fkQuestao, 
+       ROUND((1 - AVG(acertos)) * 100, 2) AS porcentagem_erros
+FROM respostas_quiz
+GROUP BY fkQuestao
+ORDER BY porcentagem_erros DESC
 LIMIT 1;
